90344900775ce6b3e734e3b3dd9693a00ed89ca8
Valery Galeano <yrelav.11gp@gmail.com>
2024-08-14 07:52:45 -0500

    error de coneccion 2

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, async() =>{
     console.log(`Server is running on port ${PORT}`)
